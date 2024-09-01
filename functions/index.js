653e929743e208f083d4d45ec70bf17080bbcd81
Jagadees27 <jagadeeswaran2705@gmail.com>
2024-09-01 10:08:14 +0530

    new function for lms

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,13 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.sendReportNotification = functions.firestore
-  .document('users/{docId}/userUSG/{usgId}')
+  .document("users/{docId}/userUSG/{usgId}")
   .onUpdate(async (change, context) => {
     const oldUserUSG = change.after.data();
     const previousUserUSG = change.before.data();
-    console.log('Updated');
+    console.log("Updated");
 
     if (oldUserUSG.report === previousUserUSG.report) {
       return;
@@ -16,7 +16,7 @@ exports.sendReportNotification = functions.firestore
     const userId = context.params.docId;
     if (!userId) {
       console.log(
-        'No userId found in the USG document. Skipping notification.'
+        "No userId found in the USG document. Skipping notification."
       );
       return;
     }
@@ -24,20 +24,18 @@ exports.sendReportNotification = functions.firestore
     try {
       const userDoc = await admin
         .firestore()
-        .collection('users')
+        .collection("users")
         .doc(userId)
         .get();
 
       if (!userDoc.exists) {
-        console.log(
-          `User document ${userId} not found. Skipping notification.`
-        );
+        console.log(`User document ${userId} not found. Skipping notificatio`);
         return;
       }
       const fcmTokens = userDoc.data().fcmTokens;
       if (fcmTokens.length === 0) {
         console.log(
-          `No FCM token found for user ${userId}. Skipping notification.`
+          ` No FCM token found for user ${userId}. Skipping notification`
         );
         return;
       }
@@ -45,31 +43,34 @@ exports.sendReportNotification = functions.firestore
       const payload = {
         token: fcmTokens[0],
         notification: {
-          title: 'USG Report Updated',
-          body: 'Your USG report has been Acknowledged.',
+          title: "USG Report Updated",
+          body: "Your USG report has been Acknowledged.",
         },
       };
 
       await admin.messaging().send(payload);
       console.log(`Notification sent successfully to user ${userId}`);
     } catch (error) {
-      console.error('Error sending notification:', error);
+      console.error("Error sending notification:", error);
     }
     return null;
   });
 
 exports.sendNewUSGNotification = functions.firestore
-  .document('clinics/{clinicId}/usg/{usgId}')
+  .document("clinics/{clinicId}/usg/{usgId}")
   .onCreate(async (snapshot, context) => {
     const clinicId = context.params.clinicId;
     const usgId = context.params.usgId;
     const usgData = snapshot.data();
-    console.log('Created');
+
+    console.log("USG document created:", usgId);
 
     const userId = usgData.userId;
+
+    console.log(clinicId);
     if (!userId) {
       console.log(
-        'No userId found in the USG document. Skipping notification.'
+        "No userId found in the USG document. Skipping notification."
       );
       return;
     }
@@ -77,39 +78,55 @@ exports.sendNewUSGNotification = functions.firestore
     try {
       const userQuerySnapshot = await admin
         .firestore()
-        .collection('users')
-        .where('clinics', 'array-contains', clinicId)
-        .where('role', '==', 'clinic')
+        .collection("users")
+        .where("clinics", "array-contains", clinicId.toString())
+        .where("role", "==", "clinic")
         .get();
+      console.log(userQuerySnapshot.docs[0]);
 
-      if (!userQuerySnapshot.exists || userQuerySnapshot.empty) {
+      if (userQuerySnapshot.empty) {
         console.log(
-          `User document ${userId} not found. Skipping notification.`
+          ` No clinic users found for clinicId ${clinicId}. Skipping notification`
         );
         return;
       }
 
       const userDoc = userQuerySnapshot.docs[0];
       const fcmTokens = userDoc.data().fcmTokens;
-      if (fcmTokens.length === 0) {
+      if (!fcmTokens || fcmTokens.length === 0) {
         console.log(
-          `No FCM token found for user ${userId}. Skipping notification.`
+          `  No FCM token found for user ${userDoc.uid}. Skipping notification.`
         );
         return;
       }
 
-      const payload = {
-        token: fcmTokens[0],
-        notification: {
-          title: 'New USG Report',
-          body: `A new USG report (${usgId}) is created.`,
-        },
-      };
-      await admin.messaging().send(payload);
-      console.log(`Notification sent successfully to user ${userId}`);
+      const payloads = fcmTokens.map((token) => {
+        return {
+          token: token,
+          notification: {
+            title: "New USG Report",
+            body: `A new USG report (${usgId}) has been created.`,
+          },
+        };
+      });
+
+      await admin.messaging().sendEach(payloads);
+      console.log(`Notification sent successfully to user ${userDoc.uid}`);
     } catch (error) {
-      console.error('Error sending notification:', error);
+      console.error("Error sending notification:", error);
     }
 
     return null;
   });
+
+exports.createUser = functions.https.onCall(async (data, context) => {
+  try {
+    const userRecord = await admin.auth().createUser({
+      email: data.email,
+      password: data.password,
+    });
+    return { uid: userRecord.uid };
+  } catch (error) {
+    return { error: error.message };
+  }
+});
